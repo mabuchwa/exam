bbe35ef6d5d5dd0733c95be4322bdaf9f1b6835a
Maud BUCHWALTER <mabuchwa@e2r6p14.42.fr>
2016-03-17 16:39:31 +0100

    version corrigee

--- ft_rrange/ft_rrange.c
+++ ft_rrange/ft_rrange.c
@@ -5,46 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabuchwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/03/10 11:18:34 by mabuchwa          #+#    #+#             */
-/*   Updated: 2016/03/10 11:34:59 by mabuchwa         ###   ########.fr       */
+/*   Created: 2016/03/17 16:17:24 by mabuchwa          #+#    #+#             */
+/*   Updated: 2016/03/17 16:36:57 by mabuchwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 int		get_size(int start, int end)
 {
 	int		result;
 
+	result = 0;
 	if (end > start)
 		result = (end - start) + 1;
 	if (end < start)
-		result = (end + start) + 1;
+		result = (start - end) + 1;
 	return (result);
 }
 
 int		*ft_rrange(int start, int end)
 {
-	int		size;
-	int		i;
 	int		*tab;
+	int		size;
 
-	i = 0;
 	size = get_size(start, end);
 	tab = (int*)malloc(sizeof(int) * size);
-	while (i < size)
+	while (size - 1 >= 0)
 	{
-		tab[i] = start;
-		printf("%d, ", tab[i]);
-		start++;
-		i++;
+		tab[size - 1] = start;
+		if (end < start)
+			start--;
+		else
+			start++;
+		size--;
 	}
 	return (tab);
-}
-
-int		main(void)
-{
-	ft_rrange(21, 2313);
-	return (0);
 }
